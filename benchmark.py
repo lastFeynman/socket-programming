ca2f445e70614343e63ea7e0c2f674115118b837
dsx <1113553107@qq.com>
2019-11-30 14:19:28 +0800

    error handling

--- benchmark.py
+++ benchmark.py
@@ -3,18 +3,40 @@ import time
 import threading
 
 
+class ResultThread(threading.Thread):
+    def __init__(self,func,args=()):
+        super(ResultThread,self).__init__()
+        self.func = func
+        self.args = args
+    def run(self):
+        self.result = self.func(*self.args)
+    def get_result(self):
+        try:
+            return self.result
+        except Exception:
+            return None
+
+
 def send_request(tid, url):
-    with urllib.request.urlopen(url) as response:
+    try:
+        response = urllib.request.urlopen(url)
         print('request ' + str(tid) + ' has finished: ' + str(response.getcode()))
+    except Exception:
+        print('request ' + str(tid) + ' went wrong.')
+        return -1;
+
+    return 0;
 
 
 if __name__ == "__main__":
-    url = '192.168.44.128:8080'
+    # url = 'http://192.168.44.128:8080'
+    url = 'http://www.baidu.com'
     n = 10000
     threads = []
+    error = 0
 
     for i in range(n):
-        threads.append(threading.Thread(target=send_request, args=(i, url)))
+        threads.append(ResultThread(send_request, args=(i, url)))
 
     start = time.perf_counter()
 
@@ -23,8 +45,10 @@ if __name__ == "__main__":
 
     for i in range(n):
         threads[i].join()
+        if threads[i].get_result() == -1:
+            error += 1
 
     end = time.perf_counter()
-
+    print(str(error) + ' threads went wrong.')
     print('total time: ' + str(end - start))
 
