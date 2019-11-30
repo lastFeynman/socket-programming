dcec8864f67d75b9006525327729a145ce9ce642
dsx <1113553107@qq.com>
2019-11-30 13:52:27 +0800

    multi-thread benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,16 +1,30 @@
 import urllib.request
 import time
+import threading
+
+
+def send_request(tid, url):
+    with urllib.request.urlopen(url) as response:
+        print('request ' + str(tid) + ' has finished: ' + str(response.getcode()))
+
 
 if __name__ == "__main__":
     url = '192.168.44.128:8080'
+    n = 10000
+    threads = []
+
+    for i in range(n):
+        threads.append(threading.Thread(target=send_request, args=(i, url)))
 
     start = time.perf_counter()
 
-    for i in range(100):
-        with urllib.request.urlopen(url) as response:
-            html = response.read()
-            print('request ' + str(i) + ' has finished: ' + str(response.getcode()))
+    for i in range(n):
+        threads[i].start()
+
+    for i in range(n):
+        threads[i].join()
 
     end = time.perf_counter()
 
     print('total time: ' + str(end - start))
+
